1301e8a3cf1de3188fef3207ab98a724314164b4
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-04-17 00:10:22 +0330

    add dynamic products file and add some feature to app.js

--- style/app.js
+++ style/app.js
@@ -1,8 +1,59 @@
-const cartBtn = document.querySelector("cart-btn");
+import { productsData } from "./products.js";
+
+const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".cart-item-confirm");
 
+const productsDOM = document.querySelector(".products-center");
+
+// get products
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+
+// display products
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((item) => {
+      result += `<section class="product">
+      <div class="img-container">
+        <img
+          class="product-img"
+          src= ${item.imageUrl}
+          alt="p-1"
+        />
+      </div>
+      <div class="product-desc">
+        <p class="products-title">${item.title}</p>
+        <p class="product-price">${item.price} &dollar;</p>
+      </div>
+      <button class="btn add-to-cart" data-id=${item.id}><i class="fa-solid fa-cart-shopping"></i> add to cart</button>
+    </section>`;
+      productsDOM.innerHTML = result;
+    });
+  }
+}
+
+// storage
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProducts();
+  const ui = new UI();
+  ui.displayProducts(productsData);
+  Storage.saveProducts(productsData);
+});
+
+// cart items modal
 function showModalFunction() {
   backDrop.style.display = "block";
   cartModal.style.opacity = "1";
@@ -10,7 +61,7 @@ function showModalFunction() {
 }
 
 function closeModalFunction() {
-  backDrop.syle.display = "none";
+  backDrop.style.display = "none";
   cartModal.style.opacity = "0";
   cartModal.style.top = "-100%";
 }
---
+++ style/products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id:1,
+    title: "queen panel bed",
+    price: 10.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:2,
+    title: "ali panel bed",
+    price: 14.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:3,
+    title: "folan panel bed",
+    price: 7.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:4,
+    title: "kallin panel bed",
+    price: 11.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:5,
+    title: "dresser panel bed",
+    price: 4.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:6,
+    title: "couch panel bed",
+    price: 23.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:7,
+    title: "fridge panel bed",
+    price: 42.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+  {
+    id:8,
+    title: "twin panel bed",
+    price: 24.99,
+    imageUrl: "./images/bedroom-1-1500749.jpg",
+  },
+]
